ed9d4abb0918a9636c9a978d1371e9f0bd5d5c87
HenokBerhanu <henokberhanut@gmail.com>
2025-04-22 12:58:39 -0600

    updates

--- Security-Enforcement-Agent/setup_securityAgent.sh
+++ Security-Enforcement-Agent/setup_securityAgent.sh
@@ -1,5 +1,7 @@
-# docker build -t your-dockerhub-username/security-agent .
-# docker push your-dockerhub-username/security-agent
+# cd Security-Enforcement-Agent/
+# docker build -t henok/security-enforcement-agent:v1 .
+# docker tag henok/security-enforcement-agent:v1 henok28/security-enforcement-agent:v1
+# docker push henok28/security-enforcement-agent:v1
 
 # setup veth Binding
 # This makes veth-sec available in the hostNetwork namespace and veth-br-sec part of the br0 bridge.
@@ -8,15 +10,25 @@ sudo ip link set veth-br-sec up
 sudo ip link set veth-sec up
 sudo ovs-vsctl add-port br0 veth-br-sec
 
-# cd Security-Enforcement-Agent/
-# docker build -t henok28/security-enforcement-agent:latest .
-# docker push henok28/security-enforcement-agent:latest
+vagrant ssh-config MasterNode
 
-# docker build -t henok/onos-diam:v3 .
-# Login to docker
-# Logout if you are logged in to the required account
-    # docker logout
-    # docker login
-    #         put your username and password
-# docker tag henok/onos-diam:v3 henok28/onos-diam:v3
-# docker push henok28/onos-diam:v3
+# coppy from the host to the edge node
+sudo scp -i /home/henok/DREAM_1.3/Kubernetes-deplyment/.vagrant/machines/MasterNode/virtualbox/private_key \
+-P 2222 \
+~/DREAM_1.3/Security-Enforcement-Agent/security-agent-deployment.yaml \
+vagrant@127.0.0.1:/home/vagrant/
+
+kubectl create namespace edge-agents
+
+kubectl apply -f security-agent-deployment.yaml
+
+kubectl get pods -n edge-agents -o wide
+
+# Confirm veth pair exists:
+ip link show veth-sec
+
+# Confirm OVS bridge has veth-br-sec:
+sudo ovs-vsctl show
+
+
+
